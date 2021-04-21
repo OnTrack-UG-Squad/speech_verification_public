bc2b52d26f066d41cd3d5e9a043123ae61244073
Somethea Siek <sometheasiekswx@gmail.com>
2021-04-21 13:41:53 +1000

    Rewrite denoise.py to ultilize noisereduce

--- speaker_verification/denoise.py
+++ speaker_verification/denoise.py
@@ -1,130 +1,44 @@
-import math
-import numpy as np
 import os
-import wave
+import sys
+
+import noisereduce as nr
+import numpy as np
+from scipy.io import wavfile
 
 
-def nextpow2(n):
-    return np.ceil(np.log2(np.abs(n))).astype("long")
+def denoise(input_file: str) -> (np.ndarray, int):
+    """
+    Denoise audio file using noisereduce.
+    """
+
+    # TODO: Read other audio types beside .wav
+    # Read audio data
+    rate, data = wavfile.read(input_file)
+    data = data.astype(np.float64)
+
+    # Section of data that is noisy.
+    # Since we don't know, just select the whole data
+    noise_clip = data
+
+    # Perform noise reduction
+    noise_reduced_data = nr.reduce_noise(audio_clip=data, noise_clip=noise_clip, verbose=False)
+
+    return rate, noise_reduced_data
 
 
-def berouti(SNR):
-    if -5.0 <= SNR <= 20.0:
-        a = 4 - SNR * 3 / 20
-    else:
-        if SNR < -5.0:
-            a = 5
-        if SNR > 20:
-            a = 1
-    return a
+def store_wav_audio(wav_data: np.ndarray, rate: int, output_file: str):
+    """
+    Store WAV data into a .wav file.
+    """
+
+    wavfile.write(output_file, rate, wav_data.astype(np.int16))
 
 
-def berouti1(SNR):
-    if -5.0 <= SNR <= 20.0:
-        a = 3 - SNR * 2 / 20
-    else:
-        if SNR < -5.0:
-            a = 4
-        if SNR > 20:
-            a = 1
-    return a
+if __name__ == '__main__':
+    input_file = sys.argv[1]
+    rate, clean_audio = denoise(input_file)
 
-
-def find_index(x_list):
-    index_list = []
-    for i in range(len(x_list)):
-        if x_list[i] < 0:
-            index_list.append(i)
-    return index_list
-
-
-def denoise(audio_file, input_path, output_path):
-    file_name = os.path.basename(audio_file).split(".")[0]
-    input_file_path = os.path.join(input_path, audio_file)
-    output_file_path = os.path.join(output_path, file_name)
-
-    with wave.open(input_file_path, 'r') as f:
-        # (nchannels, sampwidth, framerate, nframes, comptype, compname)
-        params = f.getparams()
-        nchannels, sampwidth, framerate, nframes = params[:4]
-        str_data = f.readframes(nframes)
-
-    x = np.frombuffer(str_data, dtype=np.short)
-    len_ = 20 * framerate // 1000
-    len1 = len_ * 50 // 100
-    # The percentage of the frame that the window overlaps
-    len2 = len_ - len1
-    Thres = 3
-    Expnt = 2.0
-    beta = 0.002
-    G = 0.9
-    win = np.hamming(len_)
-    # normalization gain for overlap+add with 50% overlap
-    winGain = len2 / sum(win)
-
-    # Noise magnitude calculations - assuming that the first 5 frames is noise/silence
-    nFFT = 2 * 2 ** (nextpow2(len_))
-    noise_mean = np.zeros(nFFT)
-
-    j = 0
-    for k in range(1, 6):
-        noise_mean = noise_mean + abs(np.fft.fft(win * x[j: j + len_], nFFT))
-        j = j + len_
-    noise_mu = noise_mean / 5
-
-    # --- allocate memory and initialize various variables
-    k = 1
-    img = 1j
-    x_old = np.zeros(len1)
-    Nframes = len(x) // len2 - 1
-    xfinal = np.zeros(Nframes * len2)
-
-    # Start Processing
-    for n in range(0, Nframes):
-        # Windowing
-        insign = win * x[k - 1: k + len_ - 1]
-        # compute fourier transform of a frame
-        spec = np.fft.fft(insign, nFFT)
-        # compute the magnitude
-        sig = abs(spec)
-
-        # save the noisy phase information
-        theta = np.angle(spec)
-        SNRseg = 10 * np.log10(
-            np.linalg.norm(sig, 2) ** 2 / np.linalg.norm(noise_mu, 2) ** 2
-        )
-
-        if Expnt == 1.0:
-            alpha = berouti1(SNRseg)
-        else:
-            alpha = berouti(SNRseg)
-
-        sub_speech = sig ** Expnt - alpha * noise_mu ** Expnt
-
-        diffw = sub_speech - beta * noise_mu ** Expnt
-
-        z = find_index(diffw)
-        if len(z) > 0:
-            sub_speech[z] = beta * noise_mu[z] ** Expnt
-            # implement a simple VAD detector
-        if SNRseg < Thres:  # Update noise spectrum
-            noise_temp = G * noise_mu ** Expnt + (1 - G) * sig ** Expnt
-            noise_mu = noise_temp ** (1 / Expnt)
-        sub_speech[nFFT // 2 + 1: nFFT] = np.flipud(sub_speech[1: nFFT // 2])
-        x_phase = (sub_speech ** (1 / Expnt)) * (
-                np.array([math.cos(x) for x in theta])
-                + img * (np.array([math.sin(x) for x in theta]))
-        )
-        # take the IFFT
-
-        xi = np.fft.ifft(x_phase).real
-        # Overlap and add
-        xfinal[k - 1: k + len2 - 1] = x_old + xi[0:len1]
-        x_old = xi[0 + len1: len_]
-        k = k + len2
-
-    with wave.open(output_file_path + "-denoised.wav", "wb") as wf:
-        wf.setparams(params)
-        wave_data = (winGain * xfinal).astype(np.short)
-        wf.writeframes(wave_data.tobytes())
-        print(f"writing to {output_file_path}-denoised.wav")
+    output_folder = sys.argv[2]
+    file_name = f"{os.path.basename(input_file).split('.')[0]}_cleaned.wav"
+    output_file = os.path.join(output_folder, file_name)
+    store_wav_audio(clean_audio, rate, output_file)
